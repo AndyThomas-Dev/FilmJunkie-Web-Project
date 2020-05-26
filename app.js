dcb3285b63e6fde5f97f2497828b2943ff8a69d5
Andrew Thomas <andythomas.dev@gmail.com>
2020-05-26 21:57:23 +0100

    API integrated

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
+// Basic requirements
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var stylus = require('stylus');
 var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
 
 var app = express();
-var md5 = require("md5")
 
 // For receiving JSON in posts
 var bodyParser = require("body-parser");
@@ -19,16 +17,14 @@ var jsonParser = bodyParser.json()
 
 // For the database
 var sqlite3 = require('sqlite3');
-
-var db = require("./database.js")
-var mydb = new sqlite3.Database('./db/MyDb.db');
+var db = new sqlite3.Database('./db/MyDb.db');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // Add restful controller
-require('./EmployeeController')(app, mydb, jsonParser);
+require('./EmployeeController')(app, db, jsonParser);
 
 app.use(logger('dev'));
 app.use(stylus.middleware(path.join(__dirname, 'public')));
@@ -50,110 +46,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-app.get("/api/users", (req, res, next) => {
-    var sql = "select * from user"
-    var params = []
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":rows
-        })
-      });
-});
-
-
-app.get("/api/user/:id", (req, res, next) => {
-    var sql = "select * from user where id = ?"
-    var params = [req.params.id]
-    db.get(sql, params, (err, row) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":row
-        })
-      });
-});
-
-
-app.post("/api/user/", (req, res, next) => {
-    var errors=[]
-    if (!req.body.password){
-        errors.push("No password specified");
-    }
-    if (!req.body.email){
-        errors.push("No email specified");
-    }
-    if (errors.length){
-        res.status(400).json({"error":errors.join(",")});
-        return;
-    }
-    var data = {
-        name: req.body.name,
-        email: req.body.email,
-        password : md5(req.body.password)
-    }
-    var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
-    var params =[data.name, data.email, data.password]
-    db.run(sql, params, function (err, result) {
-        if (err){
-            res.status(400).json({"error": err.message})
-            return;
-        }
-        res.json({
-            "message": "success",
-            "data": data,
-            "id" : this.lastID
-        })
-    });
-})
-
-
-
-app.patch("/api/user/:id", (req, res, next) => {
-    var data = {
-        name: req.body.name,
-        email: req.body.email,
-        password : req.body.password ? md5(req.body.password) : undefined
-    }
-    db.run(
-        `UPDATE user set 
-           name = coalesce(?,name), 
-           email = COALESCE(?,email), 
-           password = coalesce(?,password) 
-           WHERE id = ?`,
-        [data.name, data.email, data.password, req.params.id],
-        (err, result) => {
-            if (err){
-                res.status(400).json({"error": res.message})
-                return;
-            }
-            res.json({
-                message: "success",
-                data: data
-            })
-    });
-})
-
-
-app.delete("/api/user/:id", (req, res, next) => {
-    db.run(
-        'DELETE FROM user WHERE id = ?',
-        req.params.id,
-        function (err, result) {
-            if (err){
-                res.status(400).json({"error": res.message})
-                return;
-            }
-            res.json({"message":"deleted", rows: this.changes})
-    });
-})
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var createError = require('http-errors');
+// var express = require("express")
+// var app = express()
+var db = require("./database.js")
+// var md5 = require("md5")
+// var path = require('path');
+
 router.get('/', function(req, res){
   res.render('index', {
     title: 'Home'
@@ -30,5 +37,115 @@ router.get('/template', function(req, res){
     title: 'Template'
   });
 });
+
+router.get("/api/users", (req, res, next) => {
+  var sql = "select * from user"
+  var params = []
+  db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      res.json({
+          "message":"success",
+          "data":rows
+      })
+    });
+});
+
+
+router.get("/api/user/:id", (req, res, next) => {
+  var sql = "select * from user where id = ?"
+  var params = [req.params.id]
+  db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      res.json({
+          "message":"success",
+          "data":row
+      })
+    });
+});
+
+
+router.post("/api/user/", (req, res, next) => {
+  var errors=[]
+  if (!req.body.password){
+      errors.push("No password specified");
+  }
+  if (!req.body.email){
+      errors.push("No email specified");
+  }
+  if (errors.length){
+      res.status(400).json({"error":errors.join(",")});
+      return;
+  }
+  var data = {
+      name: req.body.name,
+      email: req.body.email,
+      password : md5(req.body.password)
+  }
+  var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
+  var params =[data.name, data.email, data.password]
+  db.run(sql, params, function (err, result) {
+      if (err){
+          res.status(400).json({"error": err.message})
+          return;
+      }
+      res.json({
+          "message": "success",
+          "data": data,
+          "id" : this.lastID
+      })
+  });
+})
+
+router.patch("/api/user/:id", (req, res, next) => {
+  var data = {
+      name: req.body.name,
+      email: req.body.email,
+      password : req.body.password ? md5(req.body.password) : undefined
+  }
+  db.run(
+      `UPDATE user set 
+         name = coalesce(?,name), 
+         email = COALESCE(?,email), 
+         password = coalesce(?,password) 
+         WHERE id = ?`,
+      [data.name, data.email, data.password, req.params.id],
+      (err, result) => {
+          if (err){
+              res.status(400).json({"error": res.message})
+              return;
+          }
+          res.json({
+              message: "success",
+              data: data
+          })
+  });
+})
+
+
+router.delete("/api/user/:id", (req, res, next) => {
+  db.run(
+      'DELETE FROM user WHERE id = ?',
+      req.params.id,
+      function (err, result) {
+          if (err){
+              res.status(400).json({"error": res.message})
+              return;
+          }
+          res.json({"message":"deleted", rows: this.changes})
+  });
+})
+
+
+// Root path
+router.get("/", (req, res, next) => {
+  res.json({"message":"Ok"})
+});
+
 
 module.exports = router;
